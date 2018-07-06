88dddf873b7c77780e0f5462a10ddecad2089e2f
Hiroya Inafuku <n18002@std.it-college.ac.jp>
2018-07-06 13:00:56 +0900

    7/5の課題

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -1,15 +1,13 @@
 #　値段
 sake_n = 200
 tumami_n = 100
-tori_n = 100
 # 個数
 sake_k = 2
 tumami_k = 1
-tori_k = 2
 #　割引率
-rate = 0.8
+yakitori = (100-(100*0.2))*2
 point = 150
 # 計算
-kaikei = (sake_n * sake_k) + (tumami_n) + ((tori_n * tori_k) * rate) - (point)
+kaikei = (sake_n * sake_k) + (tumami_n) + (yakitori) - (point)
 #　結果
 print("買い物の合計は", kaikei, "円です")
